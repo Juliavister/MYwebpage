2d9efe1b076c7bb96375fd33da2e82be6fd8215e
Julia Vister <juliavister@gmail.com>
2023-03-16 17:41:30 +0100

    updated main page

---
+++ js/script.js
@@ -0,0 +1,22 @@
+$(window).on("load", function() {
+    var t = $(".portfolio-container");
+    t.isotope({
+        filter: ".new",
+        animationOptions: {
+            duration: 750,
+            easing: "linear",
+            queue: !1
+        }
+    }), $(".filters a").click(function() {
+        $(".filters .active").removeClass("active"), $(this).addClass("active");
+        var i = $(this).attr("data-filter");
+        return t.isotope({
+            filter: i,
+            animationOptions: {
+                duration: 750,
+                easing: "linear",
+                queue: !1
+            }
+        }), !1
+    });
+});
